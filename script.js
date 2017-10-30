98caf05b7b33fddcea8a8e851a92bf6bda0ae5e8
Anthony Bendas <anthony.bendas@gmail.com>
2017-10-29 21:23:22 -0600

    timer settings implemented and html tag formatting

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-var breaktime = document.getElementById("breaktime");
-var focustime = document.getElementById("focustime");
-var timer = $("#focustime").val();
 
-(".clock").val(timer);
+
+$(document).ready(function () {
+
+    var breaktime = parseInt($('#breaktime').val());
+    var focustime = parseInt($('#focustime').val());
+    var timer = parseInt($('#focustime').val());
+    var timermm, timerss;
+    console.log(timer);
+
+    $('#ftup').click(function() {
+      focustime += 1;
+      $('#focustime').val(focustime);
+    })
+    $('#ftdown').click(function() {
+      focustime -= 1;
+      $('#focustime').val(focustime);
+    })
+
+    $('#btup').click(function() {
+      breaktime += 1;
+      $('#breaktime').val(breaktime);
+    })
+    $('#btdown').click(function() {
+      breaktime -= 1;
+      $('#breaktime').val(breaktime);
+    })
+
+    $('#start').click(function() {
+      timer = focustime;
+      $('.clock').val(timer);
+    })
+
+});
