28ce9b5d748d7d1e4d92d48254c5e0b6644ffa45
Anthony Bendas <anthony.bendas@gmail.com>
2017-10-31 18:11:52 -0600

    pomodoro counter working to user settings

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
-
 $(document).ready(function () {
 
     var breaktime = parseInt($('#breaktime').val());
     var focustime = parseInt($('#focustime').val());
-    var timer = parseInt($('#focustime').val());
-    var timermm, timerss;
+    //var timer = parseInt($('#focustime').val());
+    var timermm = 13, timerss = 37;
+    var sessionComplete = false;
     console.log(timer);
+    $('.clock').val(timermm + ":" + timerss);
 
     $('#ftup').click(function() {
       focustime += 1;
@@ -26,14 +26,41 @@ $(document).ready(function () {
       $('#breaktime').val(breaktime);
     })
 
+    function startTimer() {
+      var timer = setInterval(function beginTime() {
+        timerss = timerss - 1;
+        $('.clock').val(timermm + ":" + timerss);
+        console.log(sessionComplete);
+
+        if (timermm == 0 && timerss == 0){
+          if (sessionComplete == true) {
+            clearInterval(timer);
+          }
+          timermm = breaktime;
+          sessionComplete = true;
+        }
+        if (timerss == 0 & timermm >= 0){
+          timerss = 59;
+          timermm = timermm - 1;
+        }
+      },25)
+    }
+
     $('#start').click(function() {
-        timer = focustime;
-        $('.settinggui').slideToggle(false);
-        $('.clock').val(timer);
+      if (sessionComplete == true) {
+        sessionComplete = false;
+      }
+      timermm = focustime - 1;
+      timerss = 59;
+      $('.clock').val(timermm + ":" + timerss);
+      startTimer();
+      //$('.settinggui').slideToggle(false);
     })
 
     $('#stop').click(function() {
-      $('.settinggui').slideToggle(true);
+      clearInterval(timer);
+      //$('.settinggui').slideToggle(true);
     })
+
 
 });
